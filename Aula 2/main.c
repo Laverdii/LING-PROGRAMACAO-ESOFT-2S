c6368cbcb9386ff10ba69c36cb5c1819db345bbf
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-06 11:14:51 -0300

    Add files via upload

---
+++ Aula 2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	// media da nota
+	/*
+	float a, b;
+	printf("escreva a nota do primeiro bimestre: \n");
+	scanf("%f",&a);
+	printf("escreva a nota do segundo bimestre: \n");
+	scanf("%f", &b);	
+	float x = (a + b)/2;
+	if (x>=6) {
+		printf("Voce foi aprovado com a nota: %.1f", x);
+	} else {
+		printf("Voce foi reprovado com a nota: %1.f", x);
+	} 
+	*/
+	
+	// soma
+	/*
+	int a, b;
+	printf("Escreva dois numeros para somar: \n");
+	scanf("%d %d", &a, &b);
+	printf("Seu resultado é %d", (a + b));
+	*/
+	return 0;
+}
