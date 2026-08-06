df7dede7eb2db36f562fe67b0ce7dd43cc5447c7
Felipe Laverdi <laverdifelipe@gmail.com>
2026-08-06 11:17:28 -0300

    Add files via upload

---
+++ Aula1/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("O professor e bom no gol\n");
+	printf("hoje e div 1...\n"
+	"mas e claro que...\n" 
+	"sem o Leo\n"
+	"Leonardo da Silva\n"
+	"depois que voce saiu\n"
+	"ganhamos de quatro a zero\n"
+	"qual a desculpa agora?\n"
+	"...\n"
+	"...\n"
+	"oooo Leo bate na avo (oh)\n"
+	"e da bola tem do\n");
+	return 0;
+}
+/*
+	hoje é div 1...
+	mas é claro que...
+	sem o Leo
+	Leonardo da Silva
+	depois que voce saiu
+	ganhamos de quatro a zero
+	qual a desculpa agora?
+	...
+	...
+	oooo Leo bate na avó (oh)
+	e da bola tem dó
+*/
